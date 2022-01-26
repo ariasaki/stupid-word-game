3bdd2cef732f8fd1afb7c9ac2641d018f7ccbe94
aria <ariasaki81@gmail.com>
2022-01-26 06:08:56 +0000

    Update main_unix.c

--- main_unix.c
+++ main_unix.c
@@ -8,16 +8,24 @@
 #include <time.h>
 #define MAX_word_length 40
 #define MAX_word_loaded 10
+#define easy_wave 10
+#define regular_wave 8
+#define difficult_wave 5
+
 struct words{
 char word[MAX_word_length];
 int number_of_inserted_letters;
 
 };
+
 int maxx,maxy,last_word=3; //TODO: change 3 to MAX_word_loaded-1
 struct words words[MAX_word_loaded];
+
 void read_from_file(char lines[][MAX_word_length],const char file_path[30],int first_line, int last_line);
-void borders();
+void borders(const int difficulity, const int score);
 void game_play(struct words word[],double speed);
+int menu();
+
 void my_callback_on_key_arrival(char c)
 {
 
@@ -35,7 +43,9 @@ int main()
 
   initscr();
   getmaxyx(stdscr,maxy,maxx);
-  borders();
+  borders(0,0);
+  menu();
+   borders(3,0);
   char line[MAX_word_loaded][MAX_word_length];
   read_from_file(line,"file.txt",1,4);
 
@@ -67,11 +77,23 @@ void read_from_file(char lines[][MAX_word_length],const char file_path[30],int f
       break;
   }
 }
-void borders(){
+void borders(const int difficulity, const int score){
   start_color();
   init_pair(1,COLOR_GREEN,COLOR_BLACK);
   attron(COLOR_PAIR(1)|A_DIM);
-
+   if(difficulity!=0){
+  switch(difficulity){
+      case 1:
+  	 mvwprintw( stdscr, 0, 2, "difficulity: Easy" ); 
+       break;
+      case 2:
+  	 mvwprintw( stdscr, 0, 2, "difficulity: Medium" ); 
+       break;
+       case 3:
+  	 mvwprintw( stdscr, 0, 2, "difficulity: Hard" ); 
+       break;
+  }
+  }
   for(int i=0;i<maxx;i++){
     move(1,i);
     printw("#");
@@ -99,7 +121,7 @@ void game_play(struct words word[],double speed ){
   for(int i=0; i<words_count;i++)
     current_x[i]=rand()%(maxx-60) + 30;
   while(current_y+words_count!=maxy){
-    borders();
+    borders(2,0);
     words_count=last_word+1;
     for(int i=0; i<words_count; i++){
       move(current_y+i,current_x[i]);
@@ -129,4 +151,71 @@ void game_play(struct words word[],double speed ){
     
 
   }
+}
+
+int menu(){
+init_pair(3,COLOR_YELLOW,COLOR_BLACK);
+attron(A_BLINK | COLOR_PAIR(3) | A_BOLD);
+mvprintw(maxy/2, maxx/2-22, "Hello and Welcome to WORD GAME! -By Aria Saki");
+mvprintw(maxy/2+1, maxx/2-14, "Press any key to continue...");
+refresh();
+attroff(A_BLINK | COLOR_PAIR(3)| A_BOLD);
+getch();
+system("clear");
+refresh();
+WINDOW *w;
+char list[3][7] = { "Easy", "Medium", "Hard"};
+char item[7];
+int ch, i = 0, width = 7;
+w = newwin( 5, 12, maxy/2-3, maxx/2-5 ); // create a new window
+
+box( w, 0, 0 ); // sets default borders for the window
+// now print all the menu items and highlight the first one
+mvwprintw( stdscr, maxy/2-4, maxx/2-10, "Choose the difficulity: " );
+refresh( );
+for( i=0; i<3; i++ ) {
+if( i == 0 ) 
+wattron( w, A_STANDOUT ); // highlights the first item.
+else
+wattroff( w, A_STANDOUT );
+sprintf(item, "%-7s",  list[i]);
+mvwprintw( w, i+1, 2, "%s", item );
+}
+wrefresh( w ); // update the terminal screen
+i = 0;
+noecho(); // disable echoing of characters on the screen
+keypad( w, TRUE ); // enable keyboard input for the window.
+curs_set( 0 ); // hide the default screen cursor.
+// get the input
+while(( ch = wgetch(w)) != '\n'){ 
+// right pad with spaces to make the items appear with even width.
+sprintf(item, "%-7s",  list[i]); 
+mvwprintw( w, i+1, 2, "%s", item ); 
+// use a variable to increment or decrement the value based on the input.
+switch( ch ) {
+case KEY_UP:
+i--;
+i = ( i<0 ) ? 2 : i;
+break;
+case KEY_DOWN:
+i++;
+i = ( i>2 ) ? 0 : i;
+break;
+}
+// now highlight the next item in the list.
+wattron( w, A_STANDOUT );
+sprintf(item, "%-7s",  list[i]);
+mvwprintw( w, i+1, 2, "%s", item);
+wattroff( w, A_STANDOUT );
+}
+delwin( w );
+system("clear");
+refresh();
+if(strcmp(item,"Easy"))
+return 1;
+else if(strcmp(item,"Medium"))
+return 2;
+else if(strcmp(item,"Hard"))
+return 3;
+
 }
